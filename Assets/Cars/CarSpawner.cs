28169a9c4bf6856ce283bca94da9a0eb1cd2d3cd
BraveBots041 <47528376+BraveBots041@users.noreply.github.com>
2025-12-29 02:16:57 +0200

    first commit
    first commit

--- Assets/Cars/CarSpawner.cs
+++ Assets/Cars/CarSpawner.cs
@@ -1,9 +1,12 @@
+using System.Collections.Generic;
 using Unity.Collections;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class CarSpwner : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    static List<Car> cars= new List<Car>();
     void Start()
     {
 
@@ -11,7 +14,7 @@ public class CarSpwner : MonoBehaviour
     public static float spawnInterval = 2;
 
     public static float lastSpawn = 0;
-    public static float carScale = 0.5f;
+    public static float carScale = 0.5f, carSpeed= 5;
 
     // Update is called once per frame
     public static void Update()
@@ -20,6 +23,7 @@ public class CarSpwner : MonoBehaviour
         if (lastSpawn > spawnInterval)
         {
             SpawnCar();
+            lastSpawn = 0;
         }
 
         MoveCars();
@@ -27,7 +31,21 @@ public class CarSpwner : MonoBehaviour
     public static void MoveCars()
     {
 
+        foreach(Car car in cars)
+        {
+            Vector3 initialPos = car.car.transform.position;
+            float initialZ = initialPos.z;
+            if (car.movingForward)
+                car.car.transform.position = new Vector3(initialPos.x, initialPos.y, initialZ + carSpeed * Time.deltaTime);
+            else
+                car.car.transform.position = new Vector3(initialPos.x, initialPos.y, initialZ - carSpeed * Time.deltaTime);
 
+            if(car.car.transform.position.z< 0 || car.car.transform.position.z> 50)
+            {
+                Destroy(car.car);
+                cars.Remove(car);
+            }
+        }
 
     }
     public static void SpawnCar()
@@ -38,12 +56,12 @@ public class CarSpwner : MonoBehaviour
         Quaternion rotation = Quaternion.identity;
         if (direction == 0)
         {
-            spawnPose.x = 4;
+            spawnPose.x = 1;
             spawnPose.z = 0;
         }
         else
         {
-            spawnPose.x = 1;
+            spawnPose.x = 4;
             spawnPose.z = 50;
             rotation = Quaternion.Euler(0, 180, 0);
         }
